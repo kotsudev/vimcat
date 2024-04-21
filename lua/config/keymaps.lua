2bfeaca5386f44103610e2beea41fdfc54b0d077
kotsu <anton@main.home>
2024-04-21 14:11:15 +0200

    Add some usefull keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- move selected part of code
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- move last copied value into void registry afrer pasting something
+vim.keymap.set("x", "<leader>P", '"_dP')
+
+-- move copied value into void registry after deleting something
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
